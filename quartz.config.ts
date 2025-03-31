a219a4af8d9da5e678279fb237903e1b4a3c7da5
Bonnie Yang <bonnie@Mac.lan>
2025-03-31 13:46:46 -0400

    Quartz sync: Mar 31, 2025, 1:46 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,15 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: [
+      "private", 
+      "templates", 
+      ".obsidian", 
+      "lecture-notes", 
+      "fleeting-notes",
+      "daily-notes",
+      "raw"
+    ],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
@@ -73,7 +81,10 @@ const config: QuartzConfig = {
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
-    filters: [Plugin.RemoveDrafts()],
+    filters: [
+      Plugin.RemoveDrafts(),
+      Plugin.ExplicitPublish()
+    ],
     emitters: [
       Plugin.AliasRedirects(),
       Plugin.ComponentResources(),
